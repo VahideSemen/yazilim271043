d3fc1d8462e33d66c56dae4cf68ba4cb8dde1de2
ARIBİLGİ <ARIBLG@DESKTOP-8DKC8E6>
2024-10-27 18:46:21 +0300

    Add project files.

---
+++ yazilim271043/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yazilim271043
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 1, 2, 3, 4, 5, 6 };
+            string[] isimler = { "Ahmet", "Mehmet", "Hüseyin", "Abdullah", "Kasım" };
+            Console.WriteLine(sayilar[0]);
+            Console.WriteLine(sayilar[5]);
+            Console.WriteLine(sayilar.Length);
+            for (int i = 0; i < 6; i++)
+            {
+                //Console.WriteLine(i);
+                Console.WriteLine(isimler[i]);
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
